5bc775b80791f982fa02cee8070d6b7d23b7a031
hero-m <miladbashiri@yahoo.com>
2025-10-18 18:26:58 +0330

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-var TRANSLATION_RATES_URL = 'assets/rates-1404h2-v3.json';
-// var TRANSLATION_RATES_URL = 'https://hero-m.github.io/translation-calculator-miniapp/assets/rates-1404h2-v2.json';
+var TRANSLATION_RATES_URL = 'assets/rates-1404h2-v4.json';
+// var TRANSLATION_RATES_URL = 'https://hero-m.github.io/translation-calculator-miniapp/assets/rates-1404h2-v4.json';
 
 var CURRENCY = 'تومان';
 
